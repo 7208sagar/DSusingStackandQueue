e6867d9d1a7bc73241ea9b5ff54e7189567a02c4
sagar <7208sk@gmail.com>
2021-10-20 12:49:17 +0530

    Creating master Branch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSusingStackandQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack and Queue");
+        }
+    }
+}
